3e806b74d3988585eff209833acf53650b68e69f
Matt McCormick <mbmccormick@gmail.com>
2011-09-26 08:18:48 -0400

    Adjusting indentation.

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.post h2 a').click(function() {
+		alert("Testing");
+		history.pushState({ path: this.path }, '', this.href);
+		$.get(this.href, function(data) {
+			$('.listing').slideTo(data)      
+		});
+		return false; 
+	});
+
+	$(window).bind('popstate', function() {
+		$('.listing').slideTo(location.pathname);
+	});
+});
+
+slideTo:function(a) {
+	$(this).html(a);
+}
