dc529fd93a4994bb9395bcd72a9ba227b8465dfe
Matt McCormick <mbmccormick@gmail.com>
2011-09-25 20:47:17 -0400

    Testing something out!

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	$('.listing a').click(function() {
-		history.pushState({ path: this.path }, '', this.href)
+	$('.post h2 a').click(function() {
+		history.pushState({ path: this.path }, '', this.href);
 		$.get(this.href, function(data) {
 			$('.listing').slideTo(data)      
-		})
-		return false  
+		});
+		return false; 
 	});
 
 	$(window).bind('popstate', function() {
-		$('.listing').slideTo(location.pathname)
+		$('.listing').slideTo(location.pathname);
 	});
 });
 
