753618e1f355fd01eabf73c17328bcc2c5e95d17
Matt McCormick <mbmccormick@gmail.com>
2011-09-26 23:37:38 -0400

    Playing with HTML5 History API.

--- js/common.js
+++ js/common.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('article h2 a').live("click", function() {
 		history.pushState({ path: this.path }, '', this.href);
 		$(".main").load(this.href + " .main>*", function() {
-			$("html, body").animate({ scrollTop:0 }, 800);
+			$("html, body").animate({ scrollTop:0 }, 0);
 		});
 		return false; 
 	});
 
 	$(window).bind('popstate', function() {
 		$(".main").load(location.pathname + " .main>*", function() {
-			$("html, body").animate({ scrollTop:0 }, 800);
+			$("html, body").animate({ scrollTop:0 }, 0);
 		});
 	});
 });
