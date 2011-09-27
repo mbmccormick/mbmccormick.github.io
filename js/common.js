c088008276f8205c5db5a2f95f1ba1523c64816e
Matt McCormick <mbmccormick@gmail.com>
2011-09-27 15:50:40 -0300

    Edited js/common.js via GitHub

--- js/common.js
+++ js/common.js
@@ -1,46 +1,48 @@
-function supports_history_api() {
-	return !!(window.history && history.pushState);
+function slideLeft() {
+    $(".right").show();
+    $(".right").animate({ marginLeft: "310px"});
+    $(".right").addClass("main");
+    $(".right").removeClass("right");
+}
+
+function slideRight() {
+    $(".left").show();
+    $(".left").animate({ marginLeft: "310px"});
+    $(".left").addClass("main");
+    $(".left").removeClass("left");
 }
 
 $(document).ready(function() {
-	if (supports_history_api()) {
-		$('article h2 a, .pagination a').bind("click", function(e) {
-			if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
-				return true;
-				
-			history.pushState({ path: this.path }, '', this.href);
-			
-			$(".main").after("<section class='right'></section>").hide();
-			
-			$(".right").load(this.href + " .main>*", function() {
-				$("html, body").animate({ scrollTop:0 }, 0);
-			});
-			
-			$(".main").animate({ marginLeft: "-330px"})
-			$(".main").remove();
-			
-			$(".right").show();
-			$(".right").animate({ marginLeft: "310px"});
-			$(".right").addClass("main");
-			$(".right").removeClass("right");
-						
-			return false; 
-		});
+    if (!!(window.history && history.pushState)) {
+        $('article h2 a, .pagination a').live("click", function(e) {
+            if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
+                return true;
+                
+            history.pushState({ path: this.path }, '', this.href);
+            
+            $(".main").after("<section class='right'></section>").hide();
+            
+            $(".right").load(this.href + " .main>*", function() {
+                $("html, body").animate({ scrollTop:0 }, 0);
+            });
+            
+            $(".main").remove();
+            
+            slideLeft();
+                        
+            return false; 
+        });
 
-		$(window).bind('popstate', function() {
-			$(".main").after("<section class='left'></section>").hide();
-			
-			$(".left").load(location.pathname + " .main>*", function() {
-				$("html, body").animate({ scrollTop:0 }, 0);
-			});
-			
-			$(".main").animate({ marginLeft: "950px"})
-			$(".main").remove();
-			
-			$(".left").show();
-			$(".left").animate({ marginLeft: "310px"});
-			$(".left").addClass("main");
-			$(".left").removeClass("left");
-		});
-	}	
+        $(window).bind('popstate', function() {
+            $(".main").after("<section class='left'></section>").hide();
+            
+            $(".left").load(location.pathname + " .main>*", function() {
+                $("html, body").animate({ scrollTop:0 }, 0);
+            });
+            
+            $(".main").remove();
+            
+            slideRight();
+        });
+    }    
 });
