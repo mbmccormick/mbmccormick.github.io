9b9c2472038ad097ad1e5f3c7dd8d2c407fa00e1
Matt McCormick <mbmccormick@gmail.com>
2011-09-27 15:34:17 -0300

    Edited js/common.js via GitHub

--- js/common.js
+++ js/common.js
@@ -8,17 +8,21 @@ $(document).ready(function() {
 			if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
 				return true;
 				
-			var tmp = $(".main").after("<section class='right'></section>").hide();
+			$(".main").after("<section class='right'></section>").hide();
 			
 			history.pushState({ path: this.path }, '', this.href);
 			
-			$(tmp).load(this.href + " .main>*", function() {
+			$(".right").load(this.href + " .main>*", function() {
 				$("html, body").animate({ scrollTop:0 }, 0);
 			});
 			
-			$(".main").animate({ marginLeft: "-330px"}).hide().remove();
+			$(".main").animate({ marginLeft: "-330px"})
+			$(".main").remove();
 			
-			$(tmp).show().animate({ marginLeft: "310px"}).removeClass("right").addClass("main");
+			$(".right").show();
+			$(".right").animate({ marginLeft: "310px"});
+			$(".right").addClass("main");
+			$(".right").removeClass("right");
 						
 			return false; 
 		});
