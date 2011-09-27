f7244ea207dbbaa10518deb302dc03bbae3801b8
Matt McCormick <mbmccormick@gmail.com>
2011-09-27 14:58:20 -0300

    Edited js/common.js via GitHub

--- js/common.js
+++ js/common.js
@@ -7,17 +7,32 @@ $(document).ready(function() {
 		$('article h2 a, .pagination a').live("click", function(e) {
 			if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
 				return true;
+				
 			history.pushState({ path: this.path }, '', this.href);
+			
+			$(".main").fadeOut();
+			$(".loading").fadeIn();
+			
 			$(".main").load(this.href + " .main>*", function() {
 				$("html, body").animate({ scrollTop:0 }, 0);
 			});
+			
+			$(".loading").fadeOut();
+			$(".main").fadeIn();
+			
 			return false; 
 		});
 
 		$(window).bind('popstate', function() {
+			$(".main").fadeOut();
+			$(".loading").fadeIn();
+			
 			$(".main").load(location.pathname + " .main>*", function() {
 				$("html, body").animate({ scrollTop:0 }, 0);
 			});
+			
+			$(".loading").fadeOut();
+			$(".main").fadeIn();
 		});
 	}	
 });
