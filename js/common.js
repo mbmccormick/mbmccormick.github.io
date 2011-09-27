0d2da60c723b3e5bec634a6e2398cf2c24a2b326
Matt McCormick <mbmccormick@gmail.com>
2011-09-26 23:18:01 -0400

    Playing with HTML5 History API.

--- js/common.js
+++ js/common.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	$('article h2 a').live("click", function() {
-		alert("click");
-		
 		history.pushState({ path: this.path }, '', this.href);
 		$(".main").load(this.href + " .main>*", "");
 		return false; 
