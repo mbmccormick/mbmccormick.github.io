68bd8ba09e08c2693d6e8f3a66187274f2d94cc6
Matt McCormick <mbmccormick@gmail.com>
2011-09-26 23:13:26 -0400

    Starting HTML5 upgrade.

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+	alert("ready");
+	
 	$('article h2 a').click(function() {
+		alert("click");
+		
 		history.pushState({ path: this.path }, '', this.href);
 		$(".main").load(this.href + " .main>*", "");
 		return false; 
