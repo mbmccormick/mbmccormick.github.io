6e0b63786083172991ef3ee5b36cf2a6eb59e286
Matt McCormick <mbmccormick@gmail.com>
2011-09-26 23:41:13 -0400

    Playing with HTML5 History API.

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@ function supports_history_api() {
 $(document).ready(function() {
 
 	if (supports_history_api()) {
-		$('article h2 a').live("click", function() {
+		$('article h2 a, .pagination a').live("click", function() {
 			history.pushState({ path: this.path }, '', this.href);
 			$(".main").load(this.href + " .main>*", function() {
 				$("html, body").animate({ scrollTop:0 }, 0);
