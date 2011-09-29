cc218d0d7317969d2b4a71784e8a8d35157f27e0
Matt McCormick <mbmccormick@gmail.com>
2011-09-29 00:50:51 -0300

    Edited js/common.js via GitHub

--- js/common.js
+++ js/common.js
@@ -1,33 +1,59 @@
 function slideLeft() {
-    $(".main").animate({ left: "-330px"}, function() {
-        $(".main").remove();
+    $("body").css("cursor", "progress");
+            
+    $(".main").after("<section class='right'></section>");
+    
+    $(".right").load(location.href + " .main>*", function() {
+        if ($(".right h1").text().length > 0)
+            document.title = "Matt McCormick - " + $(".right h1").text();
+        else
+            document.title = "Matt McCormick";
+                        
+        $("body").css("cursor", "auto");
+        
+        $(".main").animate({ left: "-330px"}, function() {
+            $(".main").remove();
+        });
+        
+        $(".right").show();
+        $(".right").animate({ left: "310px"}, function() {
+            $(".right").addClass("main");
+            $(".right").removeClass("right");
+        });
+        
+        $("html, body").animate({ scrollTop:0 }, 0);
+        
+        $(".last").html(location.href);
     });
-    
-    $(".right").show();
-    $(".right").animate({ left: "310px"}, function() {
-        $(".right").addClass("main");
-        $(".right").removeClass("right");
-    });
-    
-    $("html, body").animate({ scrollTop:0 }, 0);
-    
-    $(".last").html(location.href);
 }
 
 function slideRight() {
-    $(".main").animate({ left: "950px"}, function() {
-        $(".main").remove();
-    });
+    $("body").css("cursor", "progress");
+            
+    $(".main").after("<section class='left'></section>");
     
-    $(".left").show();
-    $(".left").animate({ left: "310px"}, function(){
-        $(".left").addClass("main");
-        $(".left").removeClass("left");
-    });
-
-    $("html, body").animate({ scrollTop:0 }, 0);
+    $(".left").load(location.pathname + " .main>*", function() {
+        if ($(".left h1").text().length > 0)
+            document.title = "Matt McCormick - " + $(".left h1").text();
+        else
+            document.title = "Matt McCormick";
+        
+        $("body").css("cursor", "auto");
+        
+        $(".main").animate({ left: "950px"}, function() {
+            $(".main").remove();
+        });
+        
+        $(".left").show();
+        $(".left").animate({ left: "310px"}, function(){
+            $(".left").addClass("main");
+            $(".left").removeClass("left");
+        });
     
-    $(".last").html(location.href);
+        $("html, body").animate({ scrollTop:0 }, 0);
+        
+        $(".last").html(location.href);
+    });
 }
 
 var isFirstExecution = true;
@@ -38,21 +64,9 @@ $(document).ready(function() {
             if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
                 return true;
             
-            $("body").css("cursor", "progress");
-            
-            $(".main").after("<section class='right'></section>");
-            
             history.pushState('', '', this.href);
             
-            $(".right").load(this.href + " .main>*", function() {
-                if ($(".right h1").text().length > 0)
-                    document.title = "Matt McCormick - " + $(".right h1").text();
-                else
-                    document.title = "Matt McCormick";
-                                
-                $("body").css("cursor", "auto");
-                slideLeft();
-            });
+            slideLeft();
             
             return false; 
         });
@@ -61,22 +75,10 @@ $(document).ready(function() {
             if ((e.which == 1 && !e.metaKey && !e.shiftKey) == false)
                 return true;
             
-            $("body").css("cursor", "progress");
-            
-            $(".main").after("<section class='left'></section>");
-            
             history.pushState('', '', this.href);
             
-            $(".left").load(location.pathname + " .main>*", function() {
-                if ($(".left h1").text().length > 0)
-                    document.title = "Matt McCormick - " + $(".left h1").text();
-                else
-                    document.title = "Matt McCormick";
-                
-                $("body").css("cursor", "auto");
-                slideRight();
-            });
-                        
+            slideRight();
+            
             return false; 
         });
 
@@ -87,34 +89,10 @@ $(document).ready(function() {
             }
             
             if (location.href.split("/").length <= $(".last").text().split("/").length) {
-                $("body").css("cursor", "progress");
-                
-                $(".main").after("<section class='left'></section>");
-                
-                $(".left").load(location.pathname + " .main>*", function() {
-                    if ($(".left h1").text().length > 0)
-                        document.title = "Matt McCormick - " + $(".left h1").text();
-                    else
-                        document.title = "Matt McCormick";
-                    
-                    $("body").css("cursor", "auto");
-                    slideRight();
-                });
+                slideRight();
             }
             else {
-                $("body").css("cursor", "progress");
-                
-                $(".main").after("<section class='right'></section>");
-                
-                $(".right").load(location.pathname + " .main>*", function() {
-                    if ($(".right h1").text().length > 0)
-                        document.title = "Matt McCormick - " + $(".right h1").text();
-                    else
-                        document.title = "Matt McCormick";
-                    
-                    $("body").css("cursor", "auto");
-                    slideLeft();
-                });                
+                slideLeft();               
             }
         });
     }    
