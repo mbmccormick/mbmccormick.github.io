f58cc6235f19f4c37f2a7c3e2b1fbdd9e4ef3757
Matt McCormick <mbmccormick@gmail.com>
2011-10-17 16:23:13 -0300

    Update js/common.js

--- js/common.js
+++ js/common.js
@@ -82,6 +82,11 @@ var isFirstExecution = true;
 $(document).ready(function() {
     $("nav ul").each(function() {
         var list = $(this), select = $(document.createElement("select")).insertBefore($(this));
+        
+        $(document.createElement("option"))
+            .appendTo(select)
+            .html("Navigate...");
+        
         $(">li a", this).each(function() {
             var target = $(this).attr("target"),
                 option = $(document.createElement("option"))
